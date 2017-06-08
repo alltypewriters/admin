02bc6ca7719ba1db7a2a3a7ca45a166a06adca9b
shivamneeraj23 <shivamneeraj23@gmail.com>
2017-06-08 11:36:19 +0530

    Text-Editor Admin

--- adminpanel/models/story.js
+++ adminpanel/models/story.js
@@ -1,36 +1,38 @@
-
 var keystone = require('keystone');
- Types = keystone.Field.Types;
+Types = keystone.Field.Types;
 var story = new keystone.List('story');
- 
+
 story.add({
-  category: {
-    type: String
-  },
-  tags: {
-    manyStrings: { type: Types.TextArray }
-  },
-  title: {
-    type: String
-  },
-   body: {
-    
-    type:Types.Textarea
-  },
-   author: {
-    type: String
-  },
-  created_at: {
-    type: Date,
-    default: Date.now()
-  },
-   verified: {
-    type: Boolean
-  },
-   likes: {
-    type: Number
-  }
+    category: {
+        type: String
+    },
+    tags: {
+        type: Types.TextArray
+    },
+    title: {
+        type: String,
+        index: true
+    },
+    body: {
+        type: Types.Html,
+        wysiwyg: true,
+        height: 400
+    },
+    author: {
+        type: String
+    },
+    created_at: {
+        type: Date,
+        default: Date.now()
+    },
+    verified: {
+        type: Boolean
+    },
+    likes: {
+        type: Number
+    }
 
 });
- 
+
+
 story.register();
